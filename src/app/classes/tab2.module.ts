fe15cedaa609fb4f96a9db23769f56489b98a960
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-27 00:21:29 +0100

    change tab2 to classes folder

--- src/app/classes/tab2.module.ts
+++ src/app/classes/tab2.module.ts
@@ -1,11 +1,10 @@
-import { AntdModule } from './../antd.module';
+import { AntdModule } from '../antd.module';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -14,7 +13,6 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab2PageRoutingModule,
     AntdModule
   ],
