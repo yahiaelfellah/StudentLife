56822bad59a928c270c22bd7f61a80121db94797
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-26 23:02:04 +0100

    update user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase,AngularFireList, AngularFireObject} from "@angular/fire/database";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UserService {
+  userId: string;
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userId = user.uid;
+      }
+    });
+  }
 
-  constructor() { }
+  // Get all rehistered data for the user 
+  getDataForUser() {
+    if(this.userId){
+      return this.db.list(`classes/${this.userId}`);
+    }else {
+      return [];
+    }
+  }
+
+  // Create new class
+
 }
