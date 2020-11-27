785485e0586fec019f5e7362bfe6474cedbb8dee
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-27 15:08:49 +0100

    create task service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Task } from "../models/task.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TaskService {
+  private collectionName: string = "tasks";
+  constructor(private firestore: AngularFirestore) {}
+
+  createTask(task: Task) {
+    task.id = this.firestore.createId();
+    return this.firestore.collection(this.collectionName).add(task);
+  }
+
+  getTasks(id?: string) {
+    return id
+      ? this.firestore.collection<Task>(this.collectionName).valueChanges()
+      : this.firestore
+          .collection<Task>(this.collectionName)
+          .doc<Task>(id)
+          .valueChanges();
+  }
+
+  deleteTask(id: string) {
+    return this.firestore
+      .collection<Task>(this.collectionName)
+      .doc<Task>(id)
+      .delete();
+  }
+  editTask(id: string, taskUpdate: Task): Promise<void> {
+    return this.firestore
+      .collection<Task>(this.collectionName)
+      .doc<Task>(id)
+      .update(taskUpdate);
+  }
+}
