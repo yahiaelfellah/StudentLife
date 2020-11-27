082709f3b5f4a7612a2941f6f2aea2a33641f043
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-27 15:09:41 +0100

    create class model

---
+++ src/app/models/class.model.ts
@@ -0,0 +1,12 @@
+import { Time } from "@angular/common";
+
+export class Class {
+    id?:string;
+    title: string;
+    startTime: Time;
+    endTime:Time;
+    description:string;
+    platform:string;
+    days:string[];
+
+}
