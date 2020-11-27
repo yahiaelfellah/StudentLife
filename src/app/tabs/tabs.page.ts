0749c5328a3a104dc14579ba6f9d69c9d29d4e0f
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-27 15:12:34 +0100

    update tabs page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ActionSheetController, ModalController } from "@ionic/angular";
+import { ModalPage } from "../components/modal/modal.page";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  selector: "app-tabs",
+  templateUrl: "tabs.page.html",
+  styleUrls: ["tabs.page.scss"],
 })
 export class TabsPage {
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public modalController: ModalController
+  ) {}
 
-  constructor() {}
+  async presentModal(prop) {
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      cssClass: 'my-custom-modal-class',
+      componentProps: {
+        ...prop
+      }
+    });
+    return await modal.present();
+  }
 
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      cssClass: "my-custom-class",
+      buttons: [
+        {
+          text: "Create Task",
+          icon: "bookmark-outline",
+          handler: () => {
+            this.presentModal({title:"Task"});
+          },
+        },
+
+        {
+          text: "Favorite",
+          icon: "heart",
+          handler: () => {
+            console.log("Favorite clicked");
+          },
+        },
+        {
+          text: "Cancel",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 }
