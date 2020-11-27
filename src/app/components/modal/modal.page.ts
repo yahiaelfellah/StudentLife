56a42f216c6836c847569624eaed9ce59ef53ff8
DESKTOP-TFDJSG9\yahia <yahiafellah@yahoo.fr>
2020-11-27 15:06:52 +0100

    create modal component

---
+++ src/app/components/modal/modal.page.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+  @Input() title: string;
+  constructor() { 
+    this.title = '';
+  }
+
+  ngOnInit() {
+  }
+
+}
